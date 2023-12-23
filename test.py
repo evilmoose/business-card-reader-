cc503345aca6ff2aba94ec1cc943289dd1d978ab
Evilmoose <ronnielopez@gmail.com>
2023-12-22 20:12:39 -0800

    Draw bounding box

--- test.py
+++ test.py
@@ -46,6 +46,51 @@ df.dropna(inplace=True)
 # change the dataType
 col_int = ['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf']
 
-df[col_int] = df[col_int].astype(float)
+df[col_int] = df[col_int].astype(float).astype(int)
+
 
 print(df.dtypes)
+
+# Drawing Bounding Box around each word
+image = img_cv.copy()
+level = 'page'
+
+for l, x, y, w, h, c in df[['level', 'left', 'top', 'width', 'height', 'conf']].values:
+    
+    if level == 'page':
+        if l == 1:  # page is equal to 1
+            # Draw the bounding box
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0,), 2) 
+            # cv2.rectangle(
+                # Image,
+                # Point 1,
+                # Point 2 (diagnol),
+                # Color,
+                # Line Weight) 
+        else:
+            continue
+    elif level == 'block':
+        if l == 2:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0,), 2)
+        else:
+            continue
+    elif level == 'para':
+        if l == 3:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0,), 2)
+        else:
+            continue
+    elif level == 'word':
+        if l == 4:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255,), 2)
+        else:
+            continue
+    elif level == 'line':
+        if l == 5:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0,), 2)
+        else:
+            continue
+
+# Draw the box
+cv2.imshow("bounding box", image)
+cv2.waitKey()
+cv2.destroyAllWindows()
